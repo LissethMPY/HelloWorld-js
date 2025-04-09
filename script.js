9db8e7bbd187a8af1fda0a7b9d15b58dd2f4bd55
Lisseth Pacheco <lmpacheco@uce.edu.ec>
2025-04-09 16:58:20 -0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.getElementById("mensaje").innerText = "Good morning with everyone, my name is Edgar";
+document.getElementById("mensaje").innerText = "Good morning with everyone, my name is Lisseth";
